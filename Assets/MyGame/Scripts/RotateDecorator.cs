db8b341d2c2af0737a7cc416a29a7e6c0464b7b5
Noah Zopf <noah.zopf09@htl-salzburg.ac.at>
2026-03-06 13:28:39 +0100

    Fixed the RotateDecorator

--- Assets/MyGame/Scripts/RotateDecorator.cs
+++ Assets/MyGame/Scripts/RotateDecorator.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
 
-public class Rotate : MonoBehaviour
+public class RotateDecorator : MonoBehaviour
 {
-    public Vector3 rotationSpeed = new Vector3(0f, 0f, 90f); // degrees per second
+    [Header("Rotation")]
+    [SerializeField] private Vector3 rotationSpeed = new Vector3(0f, 0f, 90f);
 
-    void Update()
+    private IMover _wrappedMover;
+
+    private void Start()
     {
+        _wrappedMover = GetComponent<IMover>();
+
+        if (_wrappedMover == null)
+            Debug.LogError("RotateDecorator requires a component implementing IMover on the same GameObject.");
+    }
+
+    private void Update()
+    {
+        _wrappedMover?.Move();
         RotateObject();
     }
 
-    public void RotateObject()
+    private void RotateObject()
     {
         transform.Rotate(rotationSpeed * Time.deltaTime);
     }
